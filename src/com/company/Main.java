e1506750b94185ea021d1ec493ebc3e74359dca3
Ihor_Hodiy <goodzila2013@gmail.com>
2020-08-26 14:36:50 +0300

    fix bags

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,54 +7,55 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("What is your name? ");
+        System.out.print("What is your name? ");
         String name = sc.nextLine();
 
-        System.out.println("Where are you live " + name);
+        String LINE = "\n\n*********************************\n\n";
+
+        System.out.print("Where are you live " + name + "? ");
         String address = sc.nextLine();
-        System.out.println("Your name is: " + name + ", you live in: " + address);
-        System.out.println("*********************************");
+        System.out.print("Your name is: " + name + ", you live in: " + address);
+        System.out.print(LINE);
 
         float a, b;
-        System.out.println("Input first number ");
-        a = sc.nextInt();
-        System.out.println("Input second number ");
-        b = sc.nextInt();
+        System.out.print("Input first number: ");
+        a = sc.nextFloat();
+        System.out.print("Input second number: ");
+        b = sc.nextFloat();
         float   c = a + b,
                 d = a - b,
                 l = a * b,
                 m = a / b;
 
-        System.out.println("Your answer is: " + (int)c + ", " + (int)d + ", " + l + ", " + m);
+        System.out.print("Your answer is: ");
+        System.out.print(" " + String.format("%.2f",c));
+        System.out.print(" " + String.format("%.2f",d));
+        System.out.print(" " + String.format("%.2f",l));
+        System.out.print(" " + String.format("%.2f",m));
 
-//        System.out.println("Your answer is: " + c);
-//        System.out.println("Your answer is: " + d);
-//        System.out.println("Your answer is: " + l);
-//        System.out.println("Your answer is: " + m);
-
-        System.out.println("*********************************");
+        System.out.print(LINE);
 
         float r;
-        System.out.println("Input perimeter: ");
-        r = sc.nextInt();
+        System.out.print("Input perimeter: ");
+        r = sc.nextFloat();
 
-        System.out.println("perimeter is: " + 2 * Math.PI * r);
-        System.out.println("Area is: " + Math.PI * (Math.pow(r, 2)));
-        System.out.println("*********************************");
+        System.out.println("Perimeter is: " + String.format("%.2f", 2 * Math.PI * r));
+        System.out.print("Area is: " + String.format("%.2f", Math.PI * (Math.pow(r, 2))));
+        System.out.print(LINE);
 
-        float c1 = 5.0f, c2 = 3.5f, c3 = 28.31f;
-        float t1 = 25f, t2 = 10f, t3 = 3f;
+        float c1 = 3.45f, c2 = 5.23f, c3 = 6.23f;
+        float t1, t2, t3;
 
-//        System.out.println("Input three tariff ");
-//        c1 = sc.nextFloat();
-//        c2 = sc.nextFloat();
-//        c3 = sc.nextFloat();
+        System.out.println("Input three times ");
+        t1 = sc.nextFloat();
+        t2 = sc.nextFloat();
+        t3 = sc.nextFloat();
 
         float   p1 = c1 * t1,
                 p2 = c2 * t2,
                 p3 = c3 * t3;
 
-        System.out.println("Separately: " + (int)Math.round(p1) + ", " + (int)Math.round(p2) + ", " + (int)Math.round(p3));
+        System.out.println("Singly: " + (float)Math.round(p1) + ", " + (float)Math.round(p2) + ", " + (float)Math.round(p3));
         System.out.println("Together: " + (p1 + p2 + p3));
     }
 }
