13ca935ab81a7515ed3f4a56fb99bd7d9c638529
sagittariusdark <ms.andriy@gmail.com>
2020-08-27 16:00:44 +0300

    Some characters have been edited

--- src/com/softserve/edu/homework/Phone_calls.java
+++ src/com/softserve/edu/homework/Phone_calls.java
@@ -9,21 +9,21 @@ public class Phone_calls {
 		fromUser = new Scanner (System.in);
     int c1, c2, c3, t1,t2, t3, total;
     
-    System.out.println("Enter the cost of a call from the country ¹1");
+    System.out.println("Enter the cost of a call from the country 1");
     c1 = fromUser.nextInt();
-    System.out.println("Enter the cost of a call from the country ¹2");
+    System.out.println("Enter the cost of a call from the country 2");
     c2 = fromUser.nextInt();
-    System.out.println("Enter the cost of a call from the country ¹3");
+    System.out.println("Enter the cost of a call from the country 3");
     c3 = fromUser.nextInt();
-    System.out.println("Enter the duration of the call from the country ¹1");
+    System.out.println("Enter the duration of the call from the country 1");
     t1 = fromUser.nextInt();
-    System.out.println("Enter the duration of the call from the country ¹2");
+    System.out.println("Enter the duration of the call from the country 2");
     t2 = fromUser.nextInt();
-    System.out.println("Enter the duration of the call from the country ¹3");
+    System.out.println("Enter the duration of the call from the country 3");
     t3 = fromUser.nextInt();
-    System.out.println("The cost of a call from the country ¹1: " + c1*t1);
-    System.out.println("The cost of a call from the country ¹2: " + c2*t2);
-    System.out.println("The cost of a call from the country ¹3: " + c3*t3);
+    System.out.println("The cost of a call from the country 1: " + c1*t1);
+    System.out.println("The cost of a call from the country 2: " + c2*t2);
+    System.out.println("The cost of a call from the country 3: " + c3*t3);
     total = c1*t1+c2*t2+c3*t3;
     
     System.out.println("Total cost of calls: " + total);    
