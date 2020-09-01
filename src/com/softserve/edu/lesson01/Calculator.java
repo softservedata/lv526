9c5b0fe95a0bef698e3176efc374eaf08f2bd675
AndriyIvaniukh <andriyivaniukh@gmail.com>
2020-09-01 22:55:00 +0300

    ups, little update, calcolator.java)

--- src/com/softserve/edu/lesson01/Calculator.java
+++ src/com/softserve/edu/lesson01/Calculator.java
@@ -12,12 +12,10 @@ public class Calculator {
 		System.out.print("b = ");
 		double b;
 		b = sc.nextDouble();
-		double sum = a+b;
-		double diff = a-b;
 		System.out.println("a * b = " + a*b);
 		System.out.println("a / b = " + a/b);
-		System.out.println("a + b = " + sum);
-		System.out.println("a - b = " + diff);
+		System.out.println("a + b = " + (a+b));
+		System.out.println("a - b = " + (a-b));
 
 	}
 }
