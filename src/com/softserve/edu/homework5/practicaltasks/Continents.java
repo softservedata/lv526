0af37c24ebfb4a30dabc4db749763d5cd3d83abf
sagittariusdark <ms.andriy@gmail.com>
2020-09-08 17:32:11 +0300

    Homework&tasks

---
+++ src/com/softserve/edu/homework5/practicaltasks/Continents.java
@@ -0,0 +1,6 @@
+package com.softserve.edu.homework5.practicaltasks;
+
+public enum Continents {
+	ASIA, AFRICA, EUROPE, NORTH_AMERICA, SOUTH_AMERICA,	AUSTRALIA_OCEANIA, ANTARCTICA,UNKNOWN;
+	
+}
---
+++ src/com/softserve/edu/homework5/practicaltasks/Сountries.java
@@ -0,0 +1,47 @@
+package com.softserve.edu.homework5.practicaltasks;
+
+import java.util.Scanner;
+
+public class Ñountries {
+
+	public static void main(String[] args) {
+		String country = new String();
+		Continents continent = null;
+		System.out.println("Enter the name of country: ");
+						
+		Scanner scan = new Scanner(System.in);
+		country = scan.nextLine();
+			
+		switch (country.toLowerCase()){
+		case "china":case "india": case "pakistan": case "japan":
+		case "iran": case "vietnam": case "thailand":
+			continent = Continents.ASIA;
+		    break;
+		case "australia": case "new zealand":
+			continent = Continents.AFRICA;
+			break;
+		case "poland": case "ukraine":case"germany":case"france":
+			continent = Continents.EUROPE;
+			break;
+		case "mexico": case "canada":case"cuba":case"panama":
+			continent = Continents.NORTH_AMERICA;
+			break;
+		case "brazil": case "colombia": case "argentina": case "paraguay":case"chile":case"ecuador":
+			continent = Continents.SOUTH_AMERICA;
+			break;
+		case "samoa": case "nauru": case "niue": case"tokelau":
+			continent = Continents.AUSTRALIA_OCEANIA;
+			break;
+		case "none":
+			continent = Continents.ANTARCTICA;
+			break;
+		
+		default:
+				continent = Continents.UNKNOWN;
+		}
+		System.out.print("Countinents : " + continent);
+		scan.close();
+		    	
+  }		
+}
+
