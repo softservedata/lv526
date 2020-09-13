edff9eae6eca76d40e16072d33ca1931967ff9d7
Max Kl <Max05091998@gmail.com>
2020-09-13 16:43:28 +0300

    edu05

--- src/com/softserve/edu05/Pracktikal_task05.java
+++ src/com/softserve/edu05/Pracktikal_task05.java
@@ -11,16 +11,16 @@ public class Pracktikal_task05 {
         int count = 0;
 
         System.out.println("Enter three numbers: ");
-        for (int i = 0; i<3;i++){
+        for (int i = 0; i < 3; i++) {
             int num = sc.nextInt();
-            if(num%2!=0)
+            if (num % 2 != 0)
                 count++;
         }
-        System.out.println("Odd numbers are: "+count);
+        System.out.println("Odd numbers are: " + count);
 
         System.out.print("Enter the number of the day of the week: ");
         int b = sc.nextInt();
-        switch (b){
+        switch (b) {
             case 1:
                 System.out.println("Sunday");
                 break;
@@ -50,9 +50,9 @@ public class Pracktikal_task05 {
         System.out.println("List of country: GERMANY, USA, ARGENTINA, EGYPT, AUSTRALIA");
         System.out.print("Enter the name of the country: ");
         String c = sc.nextLine();
-        String cU= c.toUpperCase();
+        String cU = c.toUpperCase();
         Country country = Country.valueOf(cU);
-        System.out.println("Your choice: "+country.getCountry());
+        System.out.println("Your choice: " + country.getCountry());
 
     }
 
--- src/com/softserve/edu05/Product.java
+++ src/com/softserve/edu05/Product.java
@@ -5,13 +5,13 @@ public class Product {
     private int price;
     private int quantity;
 
-    public Product(String name,int price,int quantity) {
+    public Product(String name, int price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
@@ -23,7 +23,7 @@ public class Product {
         return quantity;
     }
 
-    public String toString(){
-        return "Product "+ name + "[price:" + price+", quanity: "+ quantity+"]";
+    public String toString() {
+        return "Product " + name + "[price:" + price + ", quanity: " + quantity + "]";
     }
 }
