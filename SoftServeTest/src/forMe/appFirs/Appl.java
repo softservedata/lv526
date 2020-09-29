40f511f0f4b8d917005df3e9f567d4d20ea9c064
NazariySt <struk.nazariy@gmail.com>
2020-09-29 12:01:06 +0300

    Create app Clock (test version)

---
+++ SoftServeTest/src/forMe/appFirs/Appl.java
@@ -0,0 +1,51 @@
+package forMe.appFirs;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLOutput;
+
+public class Appl extends JFrame {
+    private static final long serialVersion = 1L;
+    //
+    private JLabel tLabel = new JLabel("Time : ");
+    private JTextField tOutput = new JTextField("", 15);
+    private JButton buttonTime = new JButton(" Start ");
+    private JButton buttonExit = new JButton(" Exit ");
+    private JPanel centralPanel = new JPanel(new GridLayout(2, 2, 5, 5));
+
+
+    public Appl() {
+        super("Nazariy Clock");
+        this.setBounds(100, 100, 250, 100);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        centralPanel.add(tLabel);
+        centralPanel.add(tOutput);
+        centralPanel.add(buttonTime);
+        centralPanel.add(buttonExit);
+        Container container = this.getContentPane();
+        container.add(centralPanel, BorderLayout.CENTER);
+        //
+        buttonTime.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                long ms = System.currentTimeMillis();
+                while (System.currentTimeMillis() - ms < 5000){
+                    tOutput.setText(new Long(System.currentTimeMillis()).toString());
+                    System.out.println(tOutput.getText());
+                }
+                tOutput.setText(new Long(System.currentTimeMillis()).toString());
+                System.out.println(tOutput.getText());
+                System.out.println("buttonTime : Thread ID : " + Thread.currentThread()  );
+            }
+        });
+    buttonExit.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            setVisible(false);
+            System.exit(0);
+        }
+    });
+
+    }
+}
---
+++ SoftServeTest/src/forMe/appFirs/ApplStarter.java
@@ -0,0 +1,9 @@
+package forMe.appFirs;
+
+public class ApplStarter {
+    public static void main(String[] args) {
+        Appl apppl = new Appl();
+        apppl.setVisible(true);
+        System.out.println("main() : Thread ID = " + Thread.currentThread());
+    }
+}
