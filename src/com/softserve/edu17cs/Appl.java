b61c420d2939265b0d7a7d7b7370d101a861d818
yharasym <garasym@gmail.com>
2020-10-20 20:07:51 +0300

    Checkstyle. Debug

---
+++ src/com/softserve/edu17cs/Appl.java
@@ -0,0 +1,24 @@
+/*
+* Copyright (c) Lv-526.Java.
+* This software is code.
+*/
+package com.softserve.edu17cs;
+
+/**
+ * @author Lv-526.Java
+ */
+public final class Appl {
+
+    /**
+     * Constructor.
+     */
+    private Appl() {
+    }
+
+    /**
+     * @param args <font color="red">Arguments from <b>Command Line</b></font>.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello");
+    }
+}
